75a346af4284093dbdb43dac741e66f83672ed37
ALEX VINICIUS COSTA DA SILVA <20272488@CFP127ALUNO.LOCAL>
2021-02-03 11:17:57 -0300

    ultimo trabalho de FPOO sendo efetuado dia 03-02-2021

---
+++ Trabalho/src/br/senai/sp/jandira/model/Aluno.java
@@ -0,0 +1,82 @@
+package br.senai.sp.jandira.model;
+
+import java.time.LocalDate;
+
+public class Aluno {
+	
+	private String nome;
+	private LocalDate dataDeNascimento;
+	private char sexo;
+	private double peso;
+	private double altura;
+	private String nivelAtividade;
+	
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	
+	public void setDataDeNascimento(LocalDate DataDeNascimento) {
+		this.dataDeNascimento = DataDeNascimento;
+	}
+	
+	public LocalDate getDataDeNascimento() {
+		return this.dataDeNascimento;
+	}
+	
+	
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+	public char getSexo() {
+		return this.sexo;
+	}
+	
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public double getPeso() {
+		return this.peso;
+	}
+	
+	
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public double getAltura(double altura) {
+		return this.altura;
+	}
+	
+	
+	public void setNivelAtividade(String nivelAtividade) {
+		this.nivelAtividade = nivelAtividade;
+	}
+	
+	public String getNivelAtividade () {
+		return this.nivelAtividade;
+	}
+	
+	public void getIdade() {
+		
+	}
+	
+	public double getIMC() {
+		return peso/(Math.pow(this.altura, 2));
+	}
+	
+	
+	public double getNCD() {
+		if(idade > 59 && sexo == m || sexo == M) {
+			caloriasDiarias = 13.5 * peso + 487;
+		}
+	}
+}
